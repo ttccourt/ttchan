abe7173532b7e48af847187892e2f54eb3ce2560
ttccourt <theo@ttccourt.com>
2022-01-06 21:52:48 +0100

    Add base structure

---
+++ ttchan.py
@@ -0,0 +1,32 @@
+import flask as f
+import csv
+
+BOARD_NAME = "ttchan"
+POSTS_FILE = "posts.txt"
+
+app = f.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return f.render_template('index.html', boardname=BOARD_NAME)
+
+
+@app.route("/submit/", methods=['POST', 'GET'])
+def submit():
+    submission_success = False
+    if f.request.method == 'POST':
+        submission = [
+            f.request.form['tripcode'],
+            f.request.form['submission_text']
+        ]
+        with open(POSTS_FILE, 'a', newline='') as pf:
+            pf = csv.writer(pf)
+            pf.writerow(submission)
+            submission_success = True
+
+    return f.render_template('submit.html', boardname=BOARD_NAME, submission_success=submission_success)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80, True)
