aa21a9d8c0de2b0bf05adc7c8da028057f489755
ttccourt <theo@ttccourt.com>
2022-01-07 21:38:22 +0100

    Add images, show posts on index

--- ttchan.py
+++ ttchan.py
@@ -1,10 +1,23 @@
 import flask as f
+import os
+import uuid
 import csv
 
 BOARD_NAME = "ttchan"
 POSTS_FILE = "posts.txt"
 
 app = f.Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1000000
+app.config['UPLOAD_FOLDER'] = "images"
+
+posts = []
+
+
+class Post:
+    def __init__(self, image_name, text_content, tripcode):
+        self.image_name = image_name
+        self.text_content = text_content
+        self.tripcode = tripcode
 
 
 @app.route("/")
@@ -21,7 +34,11 @@ def favicon():
 def submit():
     submission_success = False
     if f.request.method == 'POST':
+        image_name = str(uuid.uuid4())
+        image = f.request.files['image']
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name + '.jpg'))
         submission = [
+            image_name,
             f.request.form['tripcode'],
             f.request.form['submission_text']
         ]
@@ -29,9 +46,28 @@ def submit():
             pf = csv.writer(pf)
             pf.writerow(submission)
             submission_success = True
+        posts.append(Post(*submission))
 
     return f.render_template('submit.html', boardname=BOARD_NAME, submission_success=submission_success)
 
 
+@app.route("/img/<img_uuid>")
+def get_image(img_uuid):
+    print("getting image " + img_uuid)
+    return f.send_from_directory(app.config['UPLOAD_FOLDER'], img_uuid + '.jpg')
+
+
+def loadposts():
+    try:
+        with open(POSTS_FILE, 'r') as pf:
+            pf = csv.reader(pf)
+            for post in pf:
+                posts.append(Post(*post))  # add Post object to posts, using each element in post as a param.
+    except FileNotFoundError:
+        with open(POSTS_FILE, 'x') as pf:
+            pass  # create if doesn't exist
+
+
 if __name__ == '__main__':
+    loadposts()
     app.run('0.0.0.0', 80, True)
